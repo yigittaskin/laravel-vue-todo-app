fbcfe31072e216559d863ce828a6d0a54db363f3
yigittaskin <taskinyigit2002@gmail.com>
2024-08-09 01:29:42 +0300

    fixed: scss path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List</title>
-    @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
 </head>
 <body>
     <div id="app"></div>
